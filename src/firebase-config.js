f599989436dcaa3400bde45883e08dd96af460e2
Aibin Babu <aibinniravathu@gmail.com>
2026-06-27 19:05:48 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+// Notice we added GoogleAuthProvider to this import!
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
@@ -20,3 +21,6 @@ export const auth = getAuth(app);
 
 // Initialize Cloud Firestore and export it
 export const db = getFirestore(app);
+
+// Initialize the Google Auth Provider and export it so the login button works
+export const provider = new GoogleAuthProvider();
